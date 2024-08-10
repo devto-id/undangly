40649bdc543378b9d8421081b8899a07c0503153
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2024-08-10 13:43:51 +0700

    .

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,20 +1,100 @@
 <x-app-layout>
     <div class="max-w-6xl mx-auto">
-        <h1 class="text-center text-6xl w-full py-6 leading-[1.4] font-medium">
-            Undangan Digital:
-            <br>
-            Buat dulu, <strong class="font-black">bayar</strong> kemudian
-        </h1>
+        <section>
+            <h1 class="text-center text-6xl w-full py-6 leading-[1.4] font-medium">
+                Undangan Digital:
+                <br>
+                Buat dulu, <strong class="font-black">bayar</strong> kemudian
+            </h1>
 
-        <div class="flex justify-center mt-6">
-            <a
-                class="bg-primary-600 hover:bg-primary-700 cursor-pointer text-lg font-bold text-white px-6 py-3 rounded-full shadow-lg shadow-primary-100">
-                Buat Undangan
-            </a>
-        </div>
+            <div class="flex justify-center mt-6">
+                <a
+                    class="bg-primary-600 hover:bg-primary-700 cursor-pointer text-lg font-bold text-white px-6 py-3 rounded-full shadow-lg shadow-primary-100">
+                    Buat Undangan
+                </a>
+            </div>
+        </section>
 
-        <div class="w-full mt-14">
-            <img src="/images/demo.png" class="w-full rounded-lg border-2 md:w-[90%] mx-auto" />
-        </div>
+        <section class="py-14">
+            <div class="w-full">
+                <img src="/images/demo.png" class="w-full rounded-lg border-2 md:w-[90%] mx-auto" />
+            </div>
+        </section>
+
+        <section class="py-14">
+            <h2 class="font-black text-3xl">Mengapa Undangly?</h2>
+
+            <div class="mt-8 flex gap-10 items-start">
+                <p class="w-1/2">
+                    Cepat, mudah, dan hemat.
+                    <br>
+                    <br>
+                    Undangly memberikan kemudahan dalam membuat undangan digital yang
+                    menarik dan elegan. Dengan fitur-fitur yang lengkap, kamu bisa membuat undangan digital dalam
+                    hitungan menit dengan biaya yang sangat terjangkau.
+                </p>
+
+                <div class="w-1/2 flex gap-[20px] flex-wrap">
+                    <div
+                        class="w-[calc(50%-10px)] shadow-lg shadow-neutral-50  gap-4 bg-white border p-4 rounded-lg flex flex-col items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            viewBox="0 0 20 20" class="w-[46px]">
+                            <g fill="none">
+                                <path
+                                    d="M14 7.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0a.5.5 0 0 0 1 0zM3 6a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v3.003c-.341.016-.68.092-1 .229V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8.826a.2.2 0 0 0 .34.142l.807-.796l-.002-.003l1.048-1.03l.206-.202l2.55-2.505a1.5 1.5 0 0 1 2.102 0l1.745 1.714l-.707.708l-1.739-1.709a.5.5 0 0 0-.7 0l-2.756 2.707l-1.851 1.828c-.758.748-2.043.21-2.043-.854V6zm.4 11.035c.369.184.83.335 1.217.25c.251-.056.577-.193.943-.347c.885-.373 2.003-.843 2.862-.497c.637.256.584.981.405 1.33c-.035.066-.008.16.065.177a4.6 4.6 0 0 0 1.112.088a.917.917 0 0 1 .023-.14l.375-1.498c.096-.386.296-.74.578-1.02l4.83-4.83a1.87 1.87 0 1 1 2.644 2.645l-4.83 4.829a2.197 2.197 0 0 1-1.02.578l-1.222.305c-1.121.328-2.794.222-3.313-.183c-.449-.35-.467-.887-.316-1.244c.034-.08-.026-.183-.111-.17c-.495.07-.9.25-1.3.427c-.585.26-1.156.513-1.976.411c-.711-.088-1.107-.459-1.325-.825c-.122-.204.147-.392.36-.286z"
+                                    fill="currentColor"></path>
+                            </g>
+                        </svg>
+                        Kreasikan
+                    </div>
+                    <div
+                        class="w-[calc(50%-10px)] shadow-lg shadow-neutral-50 gap-4 bg-white border p-4 rounded-lg flex flex-col items-center">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            viewBox="0 0 20 20" class="w-[46px]">
+                            <g fill="none">
+                                <path
+                                    d="M14 7.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0a.5.5 0 0 0 1 0zM3 6a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v3.003c-.341.016-.68.092-1 .229V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8.826a.2.2 0 0 0 .34.142l.807-.796l-.002-.003l1.048-1.03l.206-.202l2.55-2.505a1.5 1.5 0 0 1 2.102 0l1.745 1.714l-.707.708l-1.739-1.709a.5.5 0 0 0-.7 0l-2.756 2.707l-1.851 1.828c-.758.748-2.043.21-2.043-.854V6zm.4 11.035c.369.184.83.335 1.217.25c.251-.056.577-.193.943-.347c.885-.373 2.003-.843 2.862-.497c.637.256.584.981.405 1.33c-.035.066-.008.16.065.177a4.6 4.6 0 0 0 1.112.088a.917.917 0 0 1 .023-.14l.375-1.498c.096-.386.296-.74.578-1.02l4.83-4.83a1.87 1.87 0 1 1 2.644 2.645l-4.83 4.829a2.197 2.197 0 0 1-1.02.578l-1.222.305c-1.121.328-2.794.222-3.313-.183c-.449-.35-.467-.887-.316-1.244c.034-.08-.026-.183-.111-.17c-.495.07-.9.25-1.3.427c-.585.26-1.156.513-1.976.411c-.711-.088-1.107-.459-1.325-.825c-.122-.204.147-.392.36-.286z"
+                                    fill="currentColor"></path>
+                            </g>
+                        </svg>
+                        Bayar sekali
+                    </div>
+
+                    <div
+                        class="w-[calc(50%-10px)] shadow-lg shadow-neutral-50 gap-4 bg-white border p-4 rounded-lg flex flex-col items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            viewBox="0 0 20 20" class="w-[46px]">
+                            <g fill="none">
+                                <path
+                                    d="M14 7.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0a.5.5 0 0 0 1 0zM3 6a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v3.003c-.341.016-.68.092-1 .229V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8.826a.2.2 0 0 0 .34.142l.807-.796l-.002-.003l1.048-1.03l.206-.202l2.55-2.505a1.5 1.5 0 0 1 2.102 0l1.745 1.714l-.707.708l-1.739-1.709a.5.5 0 0 0-.7 0l-2.756 2.707l-1.851 1.828c-.758.748-2.043.21-2.043-.854V6zm.4 11.035c.369.184.83.335 1.217.25c.251-.056.577-.193.943-.347c.885-.373 2.003-.843 2.862-.497c.637.256.584.981.405 1.33c-.035.066-.008.16.065.177a4.6 4.6 0 0 0 1.112.088a.917.917 0 0 1 .023-.14l.375-1.498c.096-.386.296-.74.578-1.02l4.83-4.83a1.87 1.87 0 1 1 2.644 2.645l-4.83 4.829a2.197 2.197 0 0 1-1.02.578l-1.222.305c-1.121.328-2.794.222-3.313-.183c-.449-.35-.467-.887-.316-1.244c.034-.08-.026-.183-.111-.17c-.495.07-.9.25-1.3.427c-.585.26-1.156.513-1.976.411c-.711-.088-1.107-.459-1.325-.825c-.122-.204.147-.392.36-.286z"
+                                    fill="currentColor"></path>
+                            </g>
+                        </svg>
+                        Onlinekan selamanya
+                    </div>
+                    <div
+                        class="w-[calc(50%-10px)] shadow-lg shadow-neutral-50 gap-4 bg-white border p-4 rounded-lg flex flex-col items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            viewBox="0 0 20 20" class="w-[46px]">
+                            <g fill="none">
+                                <path
+                                    d="M14 7.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0a.5.5 0 0 0 1 0zM3 6a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v3.003c-.341.016-.68.092-1 .229V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8.826a.2.2 0 0 0 .34.142l.807-.796l-.002-.003l1.048-1.03l.206-.202l2.55-2.505a1.5 1.5 0 0 1 2.102 0l1.745 1.714l-.707.708l-1.739-1.709a.5.5 0 0 0-.7 0l-2.756 2.707l-1.851 1.828c-.758.748-2.043.21-2.043-.854V6zm.4 11.035c.369.184.83.335 1.217.25c.251-.056.577-.193.943-.347c.885-.373 2.003-.843 2.862-.497c.637.256.584.981.405 1.33c-.035.066-.008.16.065.177a4.6 4.6 0 0 0 1.112.088a.917.917 0 0 1 .023-.14l.375-1.498c.096-.386.296-.74.578-1.02l4.83-4.83a1.87 1.87 0 1 1 2.644 2.645l-4.83 4.829a2.197 2.197 0 0 1-1.02.578l-1.222.305c-1.121.328-2.794.222-3.313-.183c-.449-.35-.467-.887-.316-1.244c.034-.08-.026-.183-.111-.17c-.495.07-.9.25-1.3.427c-.585.26-1.156.513-1.976.411c-.711-.088-1.107-.459-1.325-.825c-.122-.204.147-.392.36-.286z"
+                                    fill="currentColor"></path>
+                            </g>
+                        </svg>
+                        Sebarkan
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-14">
+            <p class="text-sm text-neutral-400">
+                Copyright &copy; 2024 Undangly
+            </p>
+        </footer>
+
+
     </div>
 </x-app-layout>
