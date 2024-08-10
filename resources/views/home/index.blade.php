daf984a7d15f6f3f31c5c6a3887152a3ccc70bdb
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2024-08-10 13:10:26 +0700

    init

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <div class="max-w-6xl mx-auto">
+        <h1 class="text-center text-6xl w-full py-6 leading-[1.4] font-medium">
+            Undangan Digital:
+            <br>
+            Buat dulu, <strong class="font-black">bayar</strong> kemudian
+        </h1>
+
+        <div class="flex justify-center mt-6">
+            <a
+                class="bg-primary-600 hover:bg-primary-700 cursor-pointer text-lg font-bold text-white px-6 py-3 rounded-full shadow-lg shadow-primary-100">
+                Buat Undangan
+            </a>
+        </div>
+
+        <div class="w-full mt-14">
+            <img src="/images/demo.png" class="w-full rounded-lg border-2 md:w-[90%] mx-auto" />
+        </div>
+    </div>
+</x-app-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'primary': {
+                    DEFAULT: '#1E90FF',
+                    50: '#D6EBFF',
+                    100: '#C1E1FF',
+                    200: '#98CCFF',
+                    300: '#70B8FF',
+                    400: '#47A4FF',
+                    500: '#1E90FF',
+                    600: '#0074E5',
+                    700: '#0058AD',
+                    800: '#003B75',
+                    900: '#001F3D',
+                    950: '#001021'
+                },
+            }
+        },
+
+    },
+
+    plugins: [forms],
+};
